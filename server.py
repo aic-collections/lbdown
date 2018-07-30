8016815efb89ada96259bd46cb93a6c40765d0a7
Stefano Cossu <scossu@artic.edu>
2018-07-30 15:19:31 -0500

    Arrange files under a folder with a unique name.

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from hashlib import md5
 from os import path
 
@@ -10,6 +11,8 @@ from flask import Flask, Response, request
 CHUNK_SIZE = 4096
 """Bytes streamed at a time."""
 
+ZIPROOT_PFX = 'lake_batch_download'
+"""Beginning of the ZIP archive root name. It is suffixed with a timestamp."""
 
 logger = logging.getLogger(__name__)
 
@@ -44,7 +47,7 @@ SELECT DISTINCT ?fstype ?f ?fname {{
     aictype:IntermediateFileSet)) .
   ?fs pcdm:hasFile ?f .
   ?f ebucore:filename ?fname .
-}} LIMIT 1000
+}}
 '''
 
 
@@ -56,14 +59,16 @@ def batch_download():
     uids = request.form.get('uids', '').split(',')
     original_filename = request.form.get('original_filename', False)
 
+    ziproot = '{}-{}'.format(ZIPROOT_PFX, datetime.now().isoformat())
+
     response = Response(
-            generate(uids, original_filename), mimetype='application/zip')
+            generate(uids, ziproot, original_filename), mimetype='application/zip')
     response.headers['Content-Disposition'] = (
-            'attachment; filename={}'.format('lake_archive.zip'))
+            'attachment; filename={}.zip'.format(ziproot))
     return response
 
 
-def generate(uids, original_filename=False):
+def generate(uids, ziproot, original_filename=False):
     """
     Generate the ZIP stream.
 
@@ -113,16 +118,25 @@ def generate(uids, original_filename=False):
         role_uris = []
         for uid_el in uid_els[1:]:
             role_uris.append(role_uri_pfx + fset_types[uid_el])
-        for src in retrieve_contents(asset_uid, role_uris, original_filename):
+        for doc in retrieve_contents(asset_uid, role_uris):
             app.logger.info('Asset UID: {}'.format(asset_uid))
             app.logger.info('Fileset roles: {}'.format(role_uris))
-            zstream.write_iter(**src)
+
+            fname = (
+                    doc['orig_fname'] if original_filename
+                    else '{}-{}{}'.format(
+                        asset_uid, doc['fstype'],
+                        path.splitext(doc['orig_fname'])[1]))
+
+            arcname = '{}/{}/{}'.format(ziproot, asset_uid, fname)
+
+            zstream.write_iter(iterable=doc['iterable'], arcname=arcname)
 
     for chunk in zstream:
         yield chunk
 
 
-def retrieve_contents(asset_uid, role_uris=[], original_filename=False):
+def retrieve_contents(asset_uid, role_uris=[]):
     """
     Query the SPARQL endpoint and retrieve Fedora resources.
     """
@@ -141,19 +155,11 @@ def retrieve_contents(asset_uid, role_uris=[], original_filename=False):
 
     for doc in docs:
         file_rsp = requests.get(doc['f']['value'], stream=True)
-        orig_fname = doc['fname']['value']
-        fstype = doc['fstype']['value']
         app.logger.debug('Fedora status code: {}'.format(file_rsp.status_code))
         app.logger.debug('Fedora response headers: {}'.format(file_rsp.headers))
 
-        fname = (
-                orig_fname if original_filename
-                else '{}-{}{}'.format(
-                    asset_uid,
-                    fset_pfx[path.basename(fstype)],
-                    path.splitext(orig_fname)[1]))
-
         yield {
-            'arcname': '{}/{}'.format(asset_uid, fname),
+            'orig_fname': doc['fname']['value'],
+            'fstype': fset_pfx[path.basename(doc['fstype']['value'])],
             'iterable': file_rsp.iter_content(chunk_size=4096),
         }
